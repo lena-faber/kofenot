32ca64fe04e35f28b5d40322ae921fcde89b82bd
Lena Faber <lena1lena2@gmail.com>
2026-05-27 22:24:12 -0700

    Remove hero CTA to stat row gap

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -148,7 +148,7 @@ function Home() {
 
           {/* Stat cards and promo */}
           <Reveal delay={0.2}>
-            <div className="mt-6 flex flex-col gap-4 lg:flex-row lg:items-end lg:justify-between">
+            <div className="mt-0 flex flex-col gap-4 lg:flex-row lg:items-end lg:justify-between">
               <div className="grid grid-cols-1 sm:grid-cols-3 gap-3 max-w-xl">
                 {[
                   { big: "1 oz", small: "Flat-Folding", greenSmall: false },
