633f2006686d1f858ecf778cb42f4886f9536678
Lena Faber <lena1lena2@gmail.com>
2026-06-20 14:37:13 -0700

    Remove unnecessary whitespace in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -141,8 +141,8 @@ function Home() {
             <div className="mt-0 flex flex-col gap-4 lg:flex-row lg:items-end lg:justify-between">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 max-w-xl">
                 {[
-                  { big: "1 oz", small: "Flat-Folding", greenSmall: false },                 
-                  { big: "3-in-1", small: "Laptops Phones Tablets", greenSmall: false },
+                  { big: "1 oz", small: "Flat-Folding", greenSmall: false },
+                  { big: "3-in-1", small: "Laptops Phones Tablets", greenSmall: false }
                 ].map((p) => (
                   <div
                     key={p.big}
@@ -159,8 +159,6 @@ function Home() {
                   </div>
                 ))}
               </div>
-
-              
             </div>
           </Reveal>
         </div>
