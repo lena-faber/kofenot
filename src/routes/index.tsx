8aae279d93fc93d99d67f2141c3d1f3acd9c3056
Lena Faber <lena1lena2@gmail.com>
2026-06-20 19:34:00 -0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -181,6 +181,9 @@ function Home() {
   <p className="mt-4 text-xl font-black uppercase tracking-[0.18em] text-white md:text-2xl">
     Coffee happens. <span className="neon-text">Be ready.</span>
   </p>
+      <p className="mt-4 text-xl font-black uppercase tracking-[0.18em] text-white md:text-2xl">
+    Deflect spills. <span className="neon-text"Improve posture. </span>
+  </p>
 </Reveal>
 
     {/* CTA */}
