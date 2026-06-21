2bd48704de468bb3e0994a6957ea45348981dab1
Lena Faber <lena1lena2@gmail.com>
2026-06-21 12:53:42 -0700

    Update headline styling in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -153,8 +153,7 @@ function Home() {
 
     {/* Headline */}
     <Reveal delay={0.05}>
-     <h1 className="ml-6 mt-4 lg:text-[80px] font-black italic leading-[0.9] tracking-tight neon-text drop-shadow-[0_0_30px_rgba(0,255,0,0.35)] md:text-[8rem] lg:text-[10rem] xl:text-[12rem]">
-        ULTIMATE
+     <h1 className="ml-6 mt-4 text-[64px] md:text-[128px] lg:text-[192px] font-black italic leading-[0.9] tracking-tight neon-text drop-shadow-[0_0_30px_rgba(0,255,0,0.35)]">
         <br />
         LAPTOP
         <br />
