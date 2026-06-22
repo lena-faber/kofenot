e5f3a02906b756048eb37f83f2777c50c3cc114a
Lena Faber <lena1lena2@gmail.com>
2026-06-21 17:18:47 -0700

    Remove drop shadow from headline styling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -153,7 +153,7 @@ function Home() {
 
     {/* Headline */}
     <Reveal delay={0.05}>
-     <h1 className="ml-6 mt-4 text-[64px] md:text-[96px] lg:text-[128px] font-black italic leading-[0.9] tracking-tight neon-text drop-shadow-drop-shadow-[0_0_18px_rgba(0,255,0,0.20)]">
+     <h1 className="ml-6 mt-4 text-[64px] md:text-[96px] lg:text-[128px] font-black italic leading-[0.9] tracking-tight neon-text">
         ULTIMATE <br />
         LAPTOP
         <br />
