bd7179634d0177a46da66e8f603c1c15e32892cf
Lena Faber <lena1lena2@gmail.com>
2026-06-22 10:58:58 -0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -153,7 +153,7 @@ function Home() {
   {/* Bottom tabs */}
   <div className="absolute inset-x-0 bottom-10 z-20">
     <Reveal delay={0.2}>
-      <div className="grid h-[90px] grid-cols-4 border-t border-white/20 bg-black/10 backdrop-blur-sm">
+      <div className="grid h-[70px] grid-cols-4 border-t border-white/20 bg-black/10 backdrop-blur-sm">
         <div className="flex items-center gap-7 border-r border-white/20 px-9 h-full">
           <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2 border-[color:var(--neon)] text-4xl font-black neon-text">
             0
