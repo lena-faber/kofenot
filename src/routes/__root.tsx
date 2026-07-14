0476a0850f96000385b2a72caa826d20d6d86df3
Lena Faber <lena1lena2@gmail.com>
2026-07-13 20:35:10 -0700

    Add stylesheets to the root component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -112,7 +112,11 @@ export const Route = createRootRouteWithContext<{
       },
     ],
     links: [
-      
+      { rel: "stylesheet", href: appCss },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Caveat:wght@600;700&display=swap",
+      },
       { rel: "canonical", href: siteUrl },
       { rel: "image_src", href: siteImage },
       {
