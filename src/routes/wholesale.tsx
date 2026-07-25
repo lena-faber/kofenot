cbd7a1b99b36b8edeb08e4be266c0aa7272aaf80
Lena Faber <lena1lena2@gmail.com>
2026-07-25 10:15:12 -0700

    Update wholesale.tsx

--- src/routes/wholesale.tsx
+++ src/routes/wholesale.tsx
@@ -22,10 +22,9 @@ import coffeeShopDisplay from "@/assets/coffee-shop.jpg";
 import corpGift from "@/assets/kofenot-corp-gift.jpeg";
 import expo from "@/assets/kofenot-expo.jpeg";
 import shipping from "@/assets/kofenot-shipping.jpeg";
-
 import retailPackSipSafeBack from "@/assets/kofenot-retail-pack-00.jpg";
 import retailPackDad from "@/assets/kofenot-retail-pack-01.jpg";
-import bareProduct from "@/assets/kofenot-retail-pack-02.jpg";
+import bareProduct from "@/assets/kofenot-blank-siluette.png";
 import retailPackSipSafeFront from "@/assets/kofenot-retail-pack-03.jpg";
 import retailPackPlanet from "@/assets/kofenot-retail-pack-04.jpg";
 import yourLogoHere from "@/assets/kofenot-your.jpg";
